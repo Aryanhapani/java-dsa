d6fc37f3cff95b276f7341b4df7636656c0de500
Aryanhapani <hapaniaryan51@gmail.com>
2026-06-16 17:13:49 +0530

    first commit

---
+++ variables and datatype/comments.java
@@ -0,0 +1,11 @@
+public class comments {
+   public static void main(String[] args) {
+    int a=5;
+    int b=8;
+     
+    //adding  and b
+    int sum=a+b;
+
+    System.out.println(sum);
+   }    
+}
